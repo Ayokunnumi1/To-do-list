c078ac69867017b641dfdea6751eda5f8f669b10
Ayokunnumi1 <omololuayk@gmail.com>
2023-08-18 20:21:28 +0100

    Remove unnecessary file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import './style.css';
+import templateMarkup from './markUp';
 
 const listItemDisplayed = document.querySelector('.list-item-displayed');
 const text = document.querySelector('#text');
 const formContainer = document.querySelector('#form-container');
 const verticalDotsDiv = document.querySelector('.fa-solid fa-ellipsis-vertical');
 
-function getFromLocalStorage() {
-  const getToDoData = localStorage.getItem('todoArray');
-  if (getToDoData) {
-    return JSON.parse(getToDoData);
-  }
-  return [];
+// function getFromLocalStorage() {
+//   const toDoArray = localStorage.getItem('todoArray');
+//   if (getToDoData) {
+//     return JSON.parse(getToDoData);
+//   }
+//   return [];
+// }
+
+let toDoArray;
+if (localStorage.length !== 0) {
+  const b = JSON.parse(localStorage.getItem('todoArray'));
+  toDoArray = b;
+} else {
+  toDoArray = [];
+  localStorage.setItem('todoArray', JSON.stringify(toDoArray));
 }
 
-let toDoArray = getFromLocalStorage();
-let currentIndex = 0;
+// const toDoArray = getFromLocalStorage();
+// let currentIndex = 0;
 
 function setToLocalStorage() {
   localStorage.setItem('todoArray', JSON.stringify(toDoArray));
@@ -29,27 +39,26 @@ function getUserInput() {
     };
     toDoArray.push(userInput);
     // eslint-disable-next-line no-plusplus
-    currentIndex++;
+    // currentIndex++;
   }
   text.value = '';
 }
 
 function displayToDoList() {
-  listItemDisplayed.innerHTML = '';
-  const element = toDoArray.map((toDo, index) => {
-    const li = `<li class="list-item-container">
-                <div class="list-item">
-                    <input type="checkBox" name="checkBox" value="checkBox">
-                    <p class="check-box-text">${toDo.description}</p>
-                </div>
-                <div class="vertical-dots-div">
-                    <i class="fa-solid fa-ellipsis-vertical"></i>
-                </div>
-            </li>`;
-    return li;
+  // listItemDisplayed.innerHTML = '';
+  let markup = '';
+  toDoArray.forEach((todo) => {
+    const elemenT = templateMarkup(todo);
+    markup += elemenT;
   });
+  listItemDisplayed.innerHTML = markup;
+  // const element = toDoArray.forEach((toDo) => {
+  //   const li = templateMarkup(toDo);
+  //   return li;
+  // });
 
-  listItemDisplayed.insertAdjacentHTML('beforeend', element.join(''));
+  // listItemDisplayed.insertAdjacentHTML('beforeend', element.join(''));
+  // listItemDisplayed.innerHTML = element;
 }
 
 formContainer.addEventListener('submit', (event) => {
@@ -58,15 +67,5 @@ formContainer.addEventListener('submit', (event) => {
   setToLocalStorage();
   displayToDoList();
 });
-
-// verticalDotsDiv.addEventListener('click', () => {
-//   toDoArray = toDoArray.filter((todoArr) => (todoArr.index !== currentIndex))
-//     .map((toDoArr, id) => {
-//       toDoArr.index = id + 1;
-//       return toDoArr;
-//     });
-//   setToLocalStorage();
-//   displayToDoList();
-// });
 
 // window.addEventListener('DOMContentLoaded', displayToDoList);
