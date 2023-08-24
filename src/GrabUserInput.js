94baa1f7753e9129a3f3ac2b833728fcdea0ef4b
Ayokunnumi1 <omololuayk@gmail.com>
2023-08-24 16:22:54 +0100

    Edit GrabUserInput.js

--- src/GrabUserInput.js
+++ src/GrabUserInput.js
@@ -6,13 +6,13 @@ const todoinput = document.querySelector('.todoinput');
 class GrabUserInput {
   grab = (val) => {
     val.preventDefault();
-    if (todoinput.value === '') {
+    if (todoinput.value) {
+      const idx = JSON.parse(localStorage.getItem('todos')).length + 1;
+      const todo = new Todo(todoinput.value, idx);
+      const todosArr = JSON.parse(localStorage.getItem('todos'));
+      TodoItems.addtodo(todo, todosArr);
       return;
     }
-    const idx = JSON.parse(localStorage.getItem('todos')).length + 1;
-    const todo = new Todo(todoinput.value, idx);
-    const todosArr = JSON.parse(localStorage.getItem('todos'));
-    TodoItems.addtodo(todo, todosArr);
     todoinput.value = '';
   }
 
---
+++ src/addDeleteTest.js
@@ -0,0 +1,4 @@
+/**
+ * @jest-environment jsdom
+ */
+// import TodoItems from './TodoItems.js';
